f9f10b902e362504ca1d31a218081521fe5d14f0
ytk0701 <106838767+ytk0701@users.noreply.github.com>
2023-11-16 18:21:43 -0500

    V2 for frontend

--- frontend/app/frondtend/script.js
+++ frontend/app/frondtend/script.js
@@ -1,25 +1,28 @@
-let selectedColor = '#ff0000'; // Default color
 
-document.addEventListener("DOMContentLoaded", function() {
-  const board = document.getElementById('pixel-board');
-  const colorPicker = document.getElementById('color-picker');
-  
-  // Initialize the pixel board
-  for(let i = 0; i < 56 * 56; i++) {
-    let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixel.addEventListener('click', function() {
-      this.style.backgroundColor = selectedColor;
-    });
-    board.appendChild(pixel);
+document.addEventListener('DOMContentLoaded', function() {
+  const board = document.querySelector('.board');
+  const infoBox = document.getElementById('infoBox');
+
+  for (let i = 0; i < 56 * 56; i++) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixel.addEventListener('mouseover', function(event) {
+          showInfoBox(event, pixel);
+      });
+      board.appendChild(pixel);
   }
-
-  // Handle color selection from the color picker
-  colorPicker.addEventListener('input', function() {
-    selectedColor = this.value;
-  });
-
-  colorPicker.addEventListener('change', function() {
-    selectedColor = this.value;
-  });
 });
+
+function showInfoBox(event, pixel) {
+  const infoBox = document.getElementById('infoBox');
+  infoBox.style.display = 'block';
+  infoBox.style.left = event.pageX + 'px';
+  infoBox.style.top = event.pageY + 'px';
+}
+
+function placeBid() {
+  const colorCode = document.getElementById('colorCode').value;
+  const bidAmount = document.getElementById('bidAmount').value;
+  console.log('Color Code:', colorCode, 'Bid Amount:', bidAmount);
+  // Here, you would add the logic to handle the bid
+}
